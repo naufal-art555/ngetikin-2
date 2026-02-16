5b821d509d97048b5b0b2ae19d3494ca67305161
NAUFAL <nfalih05@gmail.com>
2026-02-16 16:09:18 +0700

    hover

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const hamburger = document.getElementById("list");
 const navbar = document.getElementById("navbar");
-const bordertext = document.getElementById("MAIN");
 
 hamburger.addEventListener("click", () => {
   navbar.classList.toggle("active");
-  bordertext.classList.toggle("active");
 });
